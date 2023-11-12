9fc13e83ccb44dfbfa45b2d2860990450418db01
Gulsara Najimova <gulsaranajimova@Gulsaras-MacBook-Pro.local>
2023-11-12 22:26:18 +0900

    Add exception classes; Create security implementing JWT

---
+++ app/core/security.py
@@ -0,0 +1,69 @@
+from datetime import timedelta, datetime
+from typing import Optional
+from fastapi import Request
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt
+from passlib.context import CryptContext
+
+from app.core.config import configs
+from app.core.exceptions import AuthError
+
+pwd_context = CryptContext(schemas=["bcrypt"], deprecated="auto")
+ALGORITHM = "HS256"
+
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_token(payload_data: dict, expires_delta: Optional[timedelta] = None):
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        access_token_expires = timedelta(configs.ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + access_token_expires
+    payload = {"exp": expire, **payload_data}
+    encoded_jwt = jwt.encode(payload, configs.SECRET_KEY, ALGORITHM)
+    expiration_datetime = expire.strftime(configs.DATETIME_FORMAT)
+    return encoded_jwt, expiration_datetime
+
+
+def decode_token(token: str):
+    try:
+        decoded_token = jwt.decode(token, configs.SECRET_KEY, ALGORITHM)
+        return decoded_token if decoded_token["exp"] >= round(datetime.utcnow().timestamp()) else None
+    except AttributeError as e:
+        return {}
+
+
+def verify_token(jwt_token: str):
+    is_token_valid: bool = False
+    try:
+        payload = decode_token(jwt_token)
+    except AttributeError as e:
+        payload = None
+    if payload:
+        is_token_valid = True
+    return is_token_valid
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise AuthError(detail="Invalid authentication scheme.")
+            if not verify_token(credentials.credentials):
+                raise AuthError(detail="Invalid token or expired token.")
+            return credentials.credentials
+        else:
+            raise AuthError(detail="Invalid authorization code.")
+
