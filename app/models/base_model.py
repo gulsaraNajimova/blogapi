b1ceb6e8181e073efa8070450d524e6f432d3fc6
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-14 11:54:36 +0900

    Create base_model.py

---
+++ app/models/base_model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Integer, DateTime, Column, func
+
+from app.core.database import Base
+
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=func.now(), nullable=True)
+    edited_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
