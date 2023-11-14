96579951cc2c90003d8e0d2e1fa5afa597e0107d
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-14 11:52:45 +0900

    Delete id, created and deleted columns and implement basemodel; create relationship with new tagsmodel; delete tags col

--- app/models/blogs_model.py
+++ app/models/blogs_model.py
@@ -1,23 +1,21 @@
 from sqlalchemy import String, Integer, Column, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
-from app.core.database import Base
+from app.models.base_model import BaseModel
+from app.models.tags_model import tag_blog_association
 
 
-class BlogsModel(Base):
+class BlogsModel(BaseModel):
     __tablename__ = "blogs"
 
-    id = Column(Integer, primary_key=True, index=True)
     title = Column(String(100))
     blog_text = Column(String)
-    tags = Column(String)
-    created_at = Column(DateTime)
-    edited_at = Column(DateTime)
     author_id = Column(Integer, ForeignKey("users.id"))
-    blog_comments_id = Column(Integer, ForeignKey("comments.id"))
+    blog_comments_id = Column(Integer, ForeignKey("comments.id"), nullable=True)
 
     owner = relationship("UserModel", back_populates="blogs")
     comments = relationship("CommentsModel", back_populates="blog")
+    tags = relationship("TagsModel", tag_blog_association, back_populates="blogs")
 
     def __repr__(self):
         return f"<Title {self.title}, tags {self.tags}>"
