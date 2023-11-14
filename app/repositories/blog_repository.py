c2bc931714b98a0f0f159996e848d78bf0c70566
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-15 00:29:21 +0900

    Add update and delete functions

--- app/repositories/blog_repository.py
+++ app/repositories/blog_repository.py
@@ -26,6 +26,11 @@ class BlogRepository:
             session.refresh(blog)
             return blog
 
+    def get_blog_by_id(self, blog_id:int):
+        with self.session_factory() as session:
+            blog = session.query(self.blog_model).filter(self.blog_model.id == bindparam("id", blog_id)).first()
+            return blog
+
     def get_blogs_by_user_id(self, author_id: int) -> List:
         with self.session_factory() as session:
             blogs = session.query(self.blog_model).filter(self.blog_model.author_id == bindparam("author_id", author_id)).all()  # noqa: W504
@@ -45,8 +50,30 @@ class BlogRepository:
             blogs = session.query(self.blog_model).join(tag_blog_association).join(TagsModel).filter(TagsModel.tag.in_(tags_to_search)).all()  # noqa: W504
             return blogs
 
-    def update_blog(self, edit_info):
+    def update_blog(self, blog_id: int, schema):
         with self.session_factory() as session:
-            edited_blog = self.blog_model(
+            blog = self.get_blog_by_id(blog_id)
+            for key, value in schema.dict(exclude_unset=True).items():
+                setattr(blog, key, value)
 
-            )
+            session.commit()
+            session.refresh(blog)
+            return blog
+
+    def update_blog_tags(self, blog_id: int, schema):
+        with self.session_factory() as session:
+            blog = self.get_blog_by_id(blog_id)
+            if schema.tags_to_add:
+                blog.tags.extend(schema.tags_to_add)
+            if schema.tags_to_delete:
+                blog.tags = [tag for tag in blog.tags if tag not in schema.tags_to_delete]
+
+            session.commit()
+            session.refresh(blog)
+            return blog
+
+    def delete_blog(self, blog_id: int):
+        with self.session_factory() as session:
+            session.query(self.blog_model).filter(self.blog_model.id == bindparam("id", blog_id)).delete()
+            session.commit()
+
