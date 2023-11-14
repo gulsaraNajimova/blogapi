51872313e97adc3814777fce183327c9e744d580
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-14 11:53:30 +0900

    Delete id and implement basemodel;

--- app/models/users_model.py
+++ app/models/users_model.py
@@ -1,13 +1,12 @@
 from sqlalchemy import String, Integer, Boolean, Column
 from sqlalchemy.orm import relationship
 
-from app.core.database import Base
+from app.models.base_model import BaseModel
 
 
-class UserModel(Base):
+class UserModel(BaseModel):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
