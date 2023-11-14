c6482cff8cfc9acc2a412c20c6ecc32bb94876b6
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-14 13:43:44 +0900

    create BaseSchema with id, created_at, and edited_at and import to auth schema; change EditBlog: include tags_to_add and tags_to_delete and include EditBlogResponse; include orm_mode = True; include SearchBlog and SearchBlogResponse schemas

--- app/schemas/blog_schema.py
+++ app/schemas/blog_schema.py
@@ -2,17 +2,20 @@ from datetime import datetime
 from typing import List, Optional
 from pydantic import BaseModel, Field
 
+from app.schemas.base_schema import BaseSchema
+
 
 class BaseBlog(BaseModel):
     title: str = Field(description="title should be under 100 characters", max_length=100)
     blog_text: str
     tags: List[str]
 
+    class Config:
+        orm_mode = True
 
-class Blog(BaseBlog):
-    id: int
+
+class Blog(BaseSchema):
     comments: List[str]
-    created_at: datetime
     author: str
     author_id: int
 
@@ -20,13 +23,25 @@ class Blog(BaseBlog):
 class EditBlog(BaseModel):
     title: Optional[str]
     blog_text: Optional[str]
+    tags_to_add: Optional[List[str]]
+    tags_to_delete: Optional[List[str]]
+
+    class Config:
+        orm_mode = True
 
 
 class EditBlogResponse(EditBlog, Blog):
-    edited_at: datetime
+    ...
 
 
-class ReadOthersBlog(BaseModel):
+class SearchBlog(BaseModel):
     author: Optional[str]
     tags: Optional[List[str]]
+
+    class Config:
+        orm_mode = True
+
+
+class SearchBlogResponse(Blog):
+    search_options: Optional[SearchBlog] = None
 
