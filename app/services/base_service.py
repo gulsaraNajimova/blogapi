04a79e235b3bfb7a1de9f161e5a329451dea39ee
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-15 17:50:37 +0900

    Create base service

---
+++ app/services/base_service.py
@@ -0,0 +1,19 @@
+from app.repositories.base_repository import BaseRepository
+
+
+class BaseService:
+    def __init__(self, repository: BaseRepository):
+        self.repository = repository
+
+    def create(self, schema):
+        return self.repository.create(schema)
+
+    def get_by_id(self, id: int, eager: bool):
+        return self.repository.read_by_id(id, eager)
+
+    def update(self, id: int, schema):
+        return self.repository.update(id, schema)
+
+    def delete(self, id: int):
+        return self.repository.delete_by_id(id)
+
