d346ae38223a31a56fe62fb291c2451828691dcd
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-15 16:52:49 +0900

    Create BaseRepository

---
+++ app/repositories/base_repository.py
@@ -0,0 +1,51 @@
+from contextlib import AbstractContextManager
+from typing import Callable
+
+from sqlalchemy import bindparam
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session, joinedload
+
+from app.core.exceptions import DuplicatedError, NotFoundError
+
+
+class BaseRepository:
+    def __init__(self, session_factory: Callable[..., AbstractContextManager[Session]], model) -> None:
+        self.session_factory = session_factory
+        self.model = model
+
+    def create(self, schema):
+        with self.session_factory() as session:
+            query = self.model(**schema.dict())
+            try:
+                session.add(query)
+                session.commit()
+                session.refresh(query)
+            except IntegrityError as e:
+                raise DuplicatedError(detail=str(e.orig))
+            return query
+
+    def read_by_id(self, id: int, eager=False):
+        with self.session_factory() as session:
+            query = session.query(self.model)
+            if eager:
+                for eager in getattr(self.model, "eagers", []):
+                    query = query.options(joinedload(getattr(self.model, eager)))
+            query = query.filter(self.model.id == bindparam("id", id)).first()
+            if not query:
+                raise NotFoundError(detail=f"Not found id : {id}")
+            return query
+
+    def update(self, id: int, schema):
+        with self.session_factory() as session:
+            session.query(self.model).filter(self.model.id == id).update(schema.dict(exclude_none=True))
+            session.commit()
+            return self.read_by_id(id)
+
+    def delete_by_id(self, id: int):
+        with self.session_factory() as session:
+            query = session.query(self.model).filter(self.model.id == id).first()
+            if not query:
+                raise NotFoundError(detail=f"Not found id : {id}")
+            session.delete(query)
+            session.commit()
+
--- app/repositories/blog_repository.py
+++ app/repositories/blog_repository.py
@@ -9,41 +9,27 @@ from app.core.exceptions import NotFoundError
 from app.models.blogs_model import BlogsModel
 from app.models.tags_model import tag_blog_association, TagsModel
 from app.models.users_model import UserModel
+from app.repositories.base_repository import BaseRepository
 
 
-class BlogRepository:
+class BlogRepository(BaseRepository):
     def __init__(self, session_factory: Callable[..., AbstractContextManager[Session]]):
         self.session_factory = session_factory
-        self.blog_model = BlogsModel
-
-    def create_blog(self, author_id: int, schema):
-        with self.session_factory() as session:
-            schema.created_at = datetime.utcnow()
-            schema.author_id = author_id
-            blog = self.blog_model(**schema.dict())
-
-            session.add(blog)
-            session.commit()
-            session.refresh(blog)
-            return blog
-
-    def get_blog_by_id(self, blog_id: int):
-        with self.session_factory() as session:
-            blog = session.query(self.blog_model).filter(self.blog_model.id == bindparam("id", blog_id)).first()
-            return blog
+        super().__init__(session_factory, BlogsModel)
 
     def get_blogs_by_user_id(self, author_id: int) -> List:
         with self.session_factory() as session:
-            blogs = session.query(self.blog_model).filter(self.blog_model.author_id == bindparam("author_id", author_id)).all()  # noqa: W504
+            blogs = session.query(self.model).filter(self.model.author_id == bindparam("author_id", author_id)).all()  # noqa: W504
             if not blogs:
                 raise NotFoundError(detail="No Blogs Found for this User")
             return blogs
 
     def search_by_author(self, username: str):
-        with self.session_factory() as session:
-            blogs = session.query(self.blog_model).join(UserModel, self.blog_model.author_id == bindparam("id", UserModel.id)
-            .filter(UserModel.username == username)
-            .options(joinedload(self.blog_model.owner)).all())  # noqa: W504
+        with (self.session_factory() as session):
+            blogs = session.query(self.model).join(UserModel, self.model.author_id == UserModel.id
+                                                   ).filter(UserModel.username == bindparam("username", username)
+                                                   .options(joinedload(self.model.owner))
+                                                   .all())  # noqa: W504
 
             if not blogs:
                 raise NotFoundError(detail="No Blogs Found for this User")
@@ -51,22 +37,12 @@ class BlogRepository:
 
     def search_by_tags(self, tags_to_search: List[str]):
         with self.session_factory() as session:
-            blogs = session.query(self.blog_model).join(tag_blog_association).join(TagsModel).filter(TagsModel.tag.in_(tags_to_search)).all()  # noqa: W504
+            blogs = session.query(self.model).join(tag_blog_association).join(TagsModel).filter(TagsModel.tag.in_(tags_to_search)).all()  # noqa: W504
             return blogs
-
-    def update_blog(self, blog_id: int, schema):
-        with self.session_factory() as session:
-            blog = self.get_blog_by_id(blog_id)
-            for key, value in schema.dict(exclude_unset=True).items():
-                setattr(blog, key, value)
-
-            session.commit()
-            session.refresh(blog)
-            return blog
 
     def update_blog_tags(self, blog_id: int, schema):
         with self.session_factory() as session:
-            blog = self.get_blog_by_id(blog_id)
+            blog = self.read_by_id(blog_id)
             if schema.tags_to_add:
                 blog.tags.extend(schema.tags_to_add)
             if schema.tags_to_delete:
@@ -75,9 +51,4 @@ class BlogRepository:
             session.commit()
             session.refresh(blog)
             return blog
-
-    def delete_blog(self, blog_id: int):
-        with self.session_factory() as session:
-            session.query(self.blog_model).filter(self.blog_model.id == bindparam("id", blog_id)).delete()
-            session.commit()
 
