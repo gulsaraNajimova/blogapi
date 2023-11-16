e1577af7703136e387239f4295ca5136aee962d0
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-16 12:45:11 +0900

    Combine search by tags, author, and all into search combined service func; create remove_duplicated function

--- app/services/blog_service.py
+++ app/services/blog_service.py
@@ -4,6 +4,19 @@ from app.repositories.blog_repository import BlogRepository
 from app.services.base_service import BaseService
 
 
+def remove_duplicates(blogs):
+    seen = set()
+    unique_blogs = []
+
+    for blog in blogs:
+        key = blog.id
+        if key not in seen:
+            seen.add(key)
+            unique_blogs.append(blog)
+
+    return unique_blogs
+
+
 class BlogService(BaseService):
     def __init__(self, blog_repository: BlogRepository):
         self.blog_repository = blog_repository
@@ -12,8 +25,19 @@ class BlogService(BaseService):
     def get_blogs_by_user_id(self, author_id: int):
         return self.blog_repository.get_blogs_by_user_id(author_id)
 
-    def search_by_author(self, username: str):
-        return self.blog_repository.search_by_author(username)
+    def search_combined(self, params):
+        blogs = []
+
+        if params.author:
+            blogs += self.blog_repository.search_by_author(username=params.author)
+        if params.tags:
+            blogs += self.blog_repository.search_by_tags(tags_to_search=params.tags)
+
+        unique_blogs = remove_duplicates(blogs)
+
+        if params.author and params.tags:
+            unique_blogs = [blog for blog in unique_blogs if blog.author == params.author]
+        return unique_blogs
 
     def search_by_tags(self, tags_to_search: List[str]):
         return self.blog_repository.search_by_tags(tags_to_search)
