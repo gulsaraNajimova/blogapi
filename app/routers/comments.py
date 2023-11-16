1b8f49f76acaaab91710bd2c82eeda4a11077ae1
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-16 12:45:40 +0900

    Modify comments routes to include service containers

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter
+from dependency_injector.wiring import Provide, inject
+from fastapi import APIRouter, Depends
 
+from app.core.containers import Container
+from app.core.dependencies import get_current_user
+from app.models.users_model import UserModel
 from app.schemas.comments_schema import Comment, BaseComment, EditCommentResponse, EditComment
+from app.services.comment_service import CommentService
 
 comment_router = APIRouter(
     prefix="/comments",
@@ -9,21 +14,32 @@ comment_router = APIRouter(
 
 
 @comment_router.post("/write-comment", response_model=Comment)
-async def write_comment(comment: BaseComment):
-    pass
+@inject
+async def write_comment(comment: BaseComment,
+                        current_user: UserModel = Depends(get_current_user),
+                        service: CommentService = Depends(Provide[Container.comment_service])):
+    return service.create(comment)
 
 
 @comment_router.get("/get-users-comments", response_model=Comment)
-async def get_my_comments(user_id: int):
-    pass
+@inject
+async def get_my_comments(current_user: UserModel = Depends(get_current_user),
+                          service: CommentService = Depends(Provide[Container.comment_service])):
+    return service.get_by_id(current_user.id, eager=False)
 
 
 @comment_router.patch("/edit_comment", response_model=EditCommentResponse)
-async def edit_comment(edit_info: EditComment, comment_id: int):
-    pass
+@inject
+async def edit_comment(edit_info: EditComment, comment_id: int,
+                       current_user: UserModel = Depends(get_current_user),
+                       service: CommentService = Depends(Provide[Container.comment_service])):
+    service.update(comment_id, edit_info)
 
 
 @comment_router.delete("/delete_comment")
-async def delete_comment(comment_id: int):
+@inject
+async def delete_comment(comment_id: int, current_user: UserModel = Depends(get_current_user),
+                         service: CommentService = Depends(Provide[Container.comment_service])):
+    service.delete(comment_id)
     return "Comment Successfully Deleted!"
 
