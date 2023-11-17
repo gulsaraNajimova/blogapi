14a9bb6dae12f0b2483ccb717326263c68cc9367
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-17 10:58:04 +0900

    Delete unused import, change author type from string to integer, modify params in def __repr__ func

--- app/models/comments_model.py
+++ app/models/comments_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, Column, DateTime, ForeignKey
+from sqlalchemy import String, Integer, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
 from app.models.base_model import BaseModel
@@ -8,12 +8,12 @@ class CommentsModel(BaseModel):
     __tablename__ = "comments"
 
     comment = Column(String)
-    author_id = Column(String, ForeignKey("users.id"), ondelete="SET NULL")
+    author_id = Column(Integer, ForeignKey("users.id"), ondelete="SET NULL")
     regarding_blog_id = Column(Integer, ForeignKey("blogs.id"))
 
     owner = relationship("UserModel", back_populates="comments")
     blog = relationship("BlogsModel", back_populates="comments")
 
     def __repr__(self):
-        return f"<Title {self.comment}, tags {self.author}>"
+        return f"<Comment {self.comment}, author {self.owner}>"
 
