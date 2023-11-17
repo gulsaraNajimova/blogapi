eb9830146b79310d6ee22af8c83691faae876604
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-17 12:50:51 +0900

    Delete ondelete attr

--- app/models/comments_model.py
+++ app/models/comments_model.py
@@ -8,7 +8,7 @@ class CommentsModel(BaseModel):
     __tablename__ = "comments"
 
     comment = Column(String)
-    author_id = Column(Integer, ForeignKey("users.id"), ondelete="SET NULL")
+    author_id = Column(Integer, ForeignKey("users.id"))
     regarding_blog_id = Column(Integer, ForeignKey("blogs.id"))
 
     owner = relationship("UserModel", back_populates="comments")
