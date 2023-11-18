462ca3658a71c9144f819178b3f11d6bcf307fb8
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-19 00:35:02 +0900

    Delete get_all_users because of same new func in the parent class; delete custom update func which is moved to user repository

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,4 +1,3 @@
-from app.core.security import hash_password
 from app.repositories.user_repository import UserRepository
 from app.services.base_service import BaseService
 
@@ -11,13 +10,4 @@ class UserService(BaseService):
     def get_by_email(self, email: str):
         return self.user_repository.get_by_email(email)
 
-    def get_all_users(self, skip: int, limit: int):
-        return self.user_repository.get_all_users(skip, limit)
-
-    def update_user_info(self, id: int, schema):
-        super().update(id, schema)
-        if "password" in schema.dict(exclude_unset=True):
-            schema.hashed_password = hash_password(schema.password)
-            delattr(schema, "password")
-        return self.user_repository.update(id, schema)
 
