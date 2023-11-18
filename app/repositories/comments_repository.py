0edf05536735ad925b764127e1ab141e3a563b53
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-19 00:35:25 +0900

    Add get_comment_by_user_id func

--- app/repositories/comments_repository.py
+++ app/repositories/comments_repository.py
@@ -1,8 +1,10 @@
 from contextlib import AbstractContextManager
 from typing import Callable
 
+from sqlalchemy import bindparam
 from sqlalchemy.orm import Session
 
+from app.core.exceptions import NotFoundError
 from app.models.comments_model import CommentsModel
 from app.repositories.base_repository import BaseRepository
 
@@ -12,3 +14,9 @@ class CommentRepository(BaseRepository):
         self.session_factory = session_factory
         super().__init__(session_factory, CommentsModel)
 
+    def get_comment_by_user_id(self, author_id: int):
+        with self.session_factory() as session:
+            comments = session.query(self.model).filter(self.model.author_id == bindparam("author_id", author_id)).all()
+            if not comments:
+                raise NotFoundError(detail="No Comments Found for this User")
+            return comments
