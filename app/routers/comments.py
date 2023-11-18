e1428ed48efb01944505f1e10de9260455e819e8
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-19 00:35:56 +0900

    Add get_all_comments route for admin

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -1,8 +1,10 @@
+from typing import List
+
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
 
 from app.core.containers import Container
-from app.core.dependencies import get_current_user
+from app.core.dependencies import get_current_user, get_current_superuser
 from app.models.users_model import UserModel
 from app.schemas.comments_schema import Comment, BaseComment, EditCommentResponse, EditComment
 from app.services.comment_service import CommentService
@@ -42,4 +44,12 @@ async def delete_comment(comment_id: int, current_user: UserModel = Depends(get_
                          service: CommentService = Depends(Provide[Container.comment_service])):
     service.delete(comment_id)
     return "Comment Successfully Deleted!"
+
+
+# for Admin
+@comment_router.get("/get_all_comments", response_model=List[Comment])
+@inject
+async def get_all_comments(skip: int = 0, limit: int = 100, current_user: UserModel = Depends(get_current_superuser),
+                           service: CommentService = Depends(Provide[Container.comment_service])):
+    return service.get_all(skip, limit)
 
