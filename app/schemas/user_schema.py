c6ed5e36efcc06e28084afe38534e6a575feebad
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-19 22:54:06 +0900

    Add from_attributes=True to BaseUser; also add BaseSchema to import from

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,11 +1,16 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr
 
+from app.schemas.base_schema import BaseSchema
 
-class BaseUser(BaseModel):
+
+class BaseUser(BaseSchema, BaseModel):
     username: str
     email: EmailStr
     is_superuser: bool
+
+    class Config:
+        from_attributes = True
 
 
 class EditUserInfo(BaseModel):
