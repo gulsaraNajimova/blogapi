54f4470233ec2563c73b14e9fe7286b88487af72
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-19 22:52:12 +0900

    Modify data types

--- app/schemas/base_schema.py
+++ app/schemas/base_schema.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class BaseSchema(BaseModel):
     id: int
-    created_at: int
-    edited_at: int
+    created_at: datetime
+    edited_at: datetime
 
