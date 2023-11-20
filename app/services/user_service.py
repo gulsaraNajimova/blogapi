8deef4fc581ce49ee52cd80f9ac1aec24375c51a
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-20 12:32:35 +0900

    Add custom update func

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,3 +1,4 @@
+from app.core.security import hash_password
 from app.repositories.user_repository import UserRepository
 from app.services.base_service import BaseService
 
@@ -8,6 +9,10 @@ class UserService(BaseService):
         super().__init__(user_repository)
 
     def update(self, id: int, schema):
+        if "password" in schema.dict(exclude_unset=True):
+            hashed_password = hash_password(schema.password)
+            setattr(schema, "hashed_password", hashed_password)
+            delattr(schema, "password")
         return self.user_repository.update(id, schema)
 
     def get_by_email(self, email: str):
