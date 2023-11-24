acc53513f52afbe951bb10d399c19101a4004d91
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-24 22:44:00 +0900

    Delete create func; Delete unused imports,

--- app/repositories/base_repository.py
+++ app/repositories/base_repository.py
@@ -1,12 +1,9 @@
 from contextlib import AbstractContextManager
 from typing import Callable
 
-from pydantic import BaseModel
-from sqlalchemy import bindparam
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.orm import Session
 
-from app.core.exceptions import DuplicatedError, NotFoundError
+from app.core.exceptions import NotFoundError
 
 
 class BaseRepository:
@@ -14,23 +11,12 @@ class BaseRepository:
         self.session_factory = session_factory
         self.model = model
 
-    def create(self, author_id: int, schema):
-        with self.session_factory() as session:
-            if isinstance(schema, BaseModel):
-                db_model = self.model(**schema.model_dump(), author_id=author_id)
-            try:
-                session.add(db_model)
-                session.commit()
-                session.refresh(db_model)
-            except IntegrityError as e:
-                raise DuplicatedError(detail=str(e.orig))
-            return db_model
-
     def read_by_id(self, id: int):
         with self.session_factory() as session:
-            query = session.query(self.model).filter(self.model.id == bindparam("id", id)).first()
+            query = session.query(self.model).filter(self.model.id == id).first()
             if not query:
                 raise NotFoundError(detail=f"Not found id : {id}")
+            print(query)
             return query
 
     def read_all(self, skip: int = 0, limit: int = 100):
@@ -39,7 +25,7 @@ class BaseRepository:
 
     def update(self, id: int, schema):
         with self.session_factory() as session:
-            session.query(self.model).filter(self.model.id == id).update(schema.dict(exclude_none=True))
+            session.query(self.model).filter(self.model.id == id).update(schema.dict(exclude_unset=True))
             session.commit()
             return self.read_by_id(id)
 
