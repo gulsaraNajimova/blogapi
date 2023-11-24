7ba47c232631c03fdb30b6dd073724c113bea69e
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-24 22:56:10 +0900

    Modify route names; Modify func names to new names accordingly; modify edit-comment route; create admin-delete-comment route

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter, Depends
 from app.core.containers import Container
 from app.core.dependencies import get_current_user, get_current_superuser
 from app.models.users_model import UserModel
-from app.schemas.comments_schema import Comment, BaseComment, EditCommentResponse, EditComment
+from app.schemas.comments_schema import Comment, BaseComment, EditComment
 from app.services.comment_service import CommentService
 
 comment_router = APIRouter(
@@ -20,22 +20,23 @@ comment_router = APIRouter(
 async def write_comment(comment: BaseComment,
                         current_user: UserModel = Depends(get_current_user),
                         service: CommentService = Depends(Provide[Container.comment_service])):
-    return service.create(comment)
+    return service.create_comment(current_user.id, comment)
 
 
-@comment_router.get("/get-users-comments", response_model=Comment)
+@comment_router.get("/get-user-comments", response_model=List[BaseComment])
 @inject
 async def get_my_comments(current_user: UserModel = Depends(get_current_user),
                           service: CommentService = Depends(Provide[Container.comment_service])):
-    return service.get_by_id(current_user.id, eager=False)
+    return service.get_user_comments(current_user.id)
 
 
-@comment_router.patch("/edit_comment", response_model=EditCommentResponse)
+@comment_router.patch("/edit_comment", response_model=Comment)
 @inject
-async def edit_comment(edit_info: EditComment, comment_id: int,
+async def edit_comment(comment_id: int, edit_info: EditComment,
                        current_user: UserModel = Depends(get_current_user),
                        service: CommentService = Depends(Provide[Container.comment_service])):
-    service.update(comment_id, edit_info)
+    comment = service.update(comment_id, edit_info)
+    return comment
 
 
 @comment_router.delete("/delete_comment")
@@ -49,7 +50,15 @@ async def delete_comment(comment_id: int, current_user: UserModel = Depends(get_
 # for Admin
 @comment_router.get("/get_all_comments", response_model=List[Comment])
 @inject
-async def get_all_comments(skip: int = 0, limit: int = 100, current_user: UserModel = Depends(get_current_superuser),
+async def admin_get_all_comments(skip: int = 0, limit: int = 100, current_user: UserModel = Depends(get_current_superuser),
                            service: CommentService = Depends(Provide[Container.comment_service])):
     return service.get_all(skip, limit)
+
+
+@comment_router.delete("delete-comment")
+@inject
+async def admin_delete_comment(comment_id: int,
+                      current_user: UserModel = Depends(get_current_superuser),
+                      service: CommentService = Depends(Provide[Container.comment_service])):
+    return service.delete(comment_id)
 
