1b1a047cdb071062b9e694f684ff0ebd24fa2777
gulsaranajimova <gulsaraxon.kayumovna@gmail.com>
2023-11-24 22:53:41 +0900

    Create create_comment func; modify func name to get_user_comments; delete unused imports

--- app/repositories/comments_repository.py
+++ app/repositories/comments_repository.py
@@ -2,9 +2,10 @@ from contextlib import AbstractContextManager
 from typing import Callable
 
 from sqlalchemy import bindparam
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
-from app.core.exceptions import NotFoundError
+from app.core.exceptions import NotFoundError, DuplicatedError
 from app.models.comments_model import CommentsModel
 from app.repositories.base_repository import BaseRepository
 
@@ -14,9 +15,18 @@ class CommentRepository(BaseRepository):
         self.session_factory = session_factory
         super().__init__(session_factory, CommentsModel)
 
-    def get_comment_by_user_id(self, author_id: int):
+    def create_comment(self, author_id: int, schema):
+        with self.session_factory() as session:
+            comment = self.model(**schema.model_dump(), author_id=author_id)
+            session.add(comment)
+            session.commit()
+            session.refresh(comment)
+            return comment
+
+    def get_user_comments(self, author_id: int):
         with self.session_factory() as session:
             comments = session.query(self.model).filter(self.model.author_id == bindparam("author_id", author_id)).all()
             if not comments:
                 raise NotFoundError(detail="No Comments Found for this User")
             return comments
+
